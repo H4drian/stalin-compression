795c75fcbfc5c55bf36dac879fcb1957a8815d05
Icosa <117495728+H4drian@users.noreply.github.com>
2024-05-29 18:00:20 -0400

    Updated Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Leon Cotten
+ * 
+ * This language is provided under the MIT Licence.
+ * See LICENSE for more information.
+ */
+
 pub fn compress_string(data: String) -> String {
     String::new()
 }
